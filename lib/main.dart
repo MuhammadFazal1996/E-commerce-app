14d12df6a8dd349fcc43adaf252a88d237916d75
mfazal <mfazal@gmail.com>
2020-12-01 08:05:44 +0500

    This is the first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/pages/login_page.dart';
+import 'package:flutter_ecommerce/pages/register_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter E-Commerce',
+      routes: {
+        '/login': (BuildContext context) => LoginPage(),
+        '/register': (BuildContext context) => RegisterPage()
+      },
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.cyan[400],
+        accentColor: Colors.deepOrange[200],
+        textTheme: TextTheme(
+            headline5: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+            headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+            bodyText2: TextStyle(fontSize: 18.0)),
+      ),
+      home: RegisterPage(),
+    );
+  }
+}
