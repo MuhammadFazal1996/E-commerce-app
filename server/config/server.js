c02a7ee9a0e4a1054dd690475619d92f274b3844
mfazal <mfazal@gmail.com>
2020-12-04 10:31:09 +0500

    strapi connection established

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c4a488b2c336651e49582a1dad295c3b'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c3ad944b-69f2-4873-9b28-d5b0a977048f'
+};
